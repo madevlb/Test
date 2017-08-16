6ebeb6cd3981e17c027929dd79875d152c146d38
lb <fromlib@163.com>
2017-08-16 16:07:24 +0800

    获取TextView控件

--- Test/app/src/main/java/com/example/libin/test/MainActivity.java
+++ Test/app/src/main/java/com/example/libin/test/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.libin.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add some code
+        txtView = (TextView) findViewById(R.id.tv);
     }
 }
